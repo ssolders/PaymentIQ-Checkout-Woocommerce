a738f8f1b7dd7c78278eee02f3062740613ad7ec
Simon Solders <simon.solders@gmail.com>
2020-05-08 09:42:22 +0200

    updates

--- inc/Utils.php
+++ inc/Utils.php
@@ -13,6 +13,40 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Echoes Checkout iframe setup
  */
 
+function piq_wc_show_checkout () {
+  $currentOrder = piq_create_or_update_order();
+}
+
+function piq_create_or_update_order( $order_id = null ) {
+	// Need to calculate these here, because WooCommerce hasn't done it yet.
+	WC()->cart->calculate_fees();
+	WC()->cart->calculate_shipping();
+	WC()->cart->calculate_totals();
+	// if ( WC()->session->get( 'kco_wc_order_id' ) ) { // Check if we have an order id.
+	// 	// Try to update the order, if it fails try to create new order.
+	// 	$klarna_order = KCO_WC()->api->update_klarna_order( WC()->session->get( 'kco_wc_order_id' ) );
+	// 	if ( ! $klarna_order ) {
+	// 		// If update order failed try to create new order.
+	// 		$klarna_order = KCO_WC()->api->create_klarna_order();
+	// 		if ( ! $klarna_order ) {
+	// 			// If failed then bail.
+	// 			return;
+	// 		}
+	// 		WC()->session->set( 'kco_wc_order_id', $klarna_order['order_id'] );
+	// 		return $klarna_order;
+	// 	}
+	// 	return $klarna_order;
+	// } else {
+	// 	// Create new order, since we dont have one.
+	// 	$klarna_order = KCO_WC()->api->create_klarna_order();
+	// 	if ( ! $klarna_order ) {
+	// 		return;
+	// 	}
+	// 	WC()->session->set( 'kco_wc_order_id', $klarna_order['order_id'] );
+	// 	return $klarna_order;
+	// }
+}
+
 function shouldSetupCheckout() {
 	if ( null !== PIQ_MID && null !== PIQ_TOTAL_AMOUNT && null !== PIQ_TOTAL_AMOUNT ) {
 		return true;
--- templates/Checkout/paymentiq-checkout.php
+++ templates/Checkout/paymentiq-checkout.php
@@ -18,6 +18,8 @@ if ( ! shouldSetupCheckout ()) {
 	return;
 }
 
+/*<?php piq_wc_show_checkout(); ?>*/
+
 // If checkout registration is disabled and not logged in, the user cannot checkout.
 // if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
 // 	echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
